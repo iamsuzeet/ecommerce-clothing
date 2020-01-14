a2272af746d4cb814da766af70f66bf3c05e3508
iamsuzeet <iamsuzeet@gmail.com>
2020-01-14 14:20:42 +0545

    implemented firebase utils, store authenticated user into firestore database

--- src/pages/sign-in-up/SignInUp.jsx
+++ src/pages/sign-in-up/SignInUp.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import SignIn from "./../../components/sign-in/SignIn";
+import SignUp from "../../components/sign-up/SignUp";
+
+import styles from "./signInUp.module.scss";
 
 const SignInUp = () => {
   return (
-    <div>
+    <div className={styles.signInUp}>
       <SignIn />
+      <SignUp />
     </div>
   );
 };
