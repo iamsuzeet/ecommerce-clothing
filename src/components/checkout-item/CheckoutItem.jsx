81b55193a577135862b94e9f36b054aa8a3a3676
iamsuzeet <iamsuzeet@gmail.com>
2020-01-27 19:13:44 +0545

    Added additem and removeitem functionality to checkoutItem component

--- src/components/checkout-item/CheckoutItem.jsx
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 import styles from "./CheckoutItem.module.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+import { connect } from "react-redux";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from "./../../redux/cart/cart-action";
+
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className={styles.CheckoutItem}>
       <div className={styles.ImageContainer}>
         <img src={imageUrl} alt="item" />
       </div>
       <div className={styles.name}>{name}</div>
-      <div className={styles.quantity}>{quantity}</div>
+      <div className={styles.quantity}>
+        <div onClick={() => removeItem(cartItem)} className={styles.arrow}>
+          &#10094;
+        </div>
+        <span className={styles.value}>{quantity}</span>
+        <div onClick={() => addItem(cartItem)} className={styles.arrow}>
+          &#10095;
+        </div>
+      </div>
       <div className={styles.price}>{price}</div>
-      <div className={styles.RemoveButton}>&#10005;</div>
+      <div onClick={() => clearItem(cartItem)} className={styles.RemoveButton}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -22,6 +22,9 @@ const Checkout = ({ cartItems, total }) => {
         </div>
         <div className={styles.HeaderBlock}>
           <span>Quantity</span>
+        </div>{" "}
+        <div className={styles.HeaderBlock}>
+          <span>Price</span>
         </div>
         <div className={styles.HeaderBlock}>
           <span>Remove</span>
